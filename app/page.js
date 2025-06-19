142078dbe47832760d5ce51e85da967cae07998c
Raghavv <raghavjayateerth@gmail.com>
2025-06-20 01:46:05 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,25 @@
+import "./page.css";
+
+export default function Home() {
+  return (
+    <>
+      <main className="main_body">
+        <div className="content_container">
+          <header className="header_container font-ui-sans-serif">
+            <h1>Raghavendra Jayateerth</h1>
+            <p>Frontend Developer</p>
+            <p className="paragraph">
+              Based in Bangalore,India with 2 years of experience in Next.js,
+              Angular, and React, I bring a unique blend of passion and
+              proficiency to development and design. Obsessed with performance
+              and dedicated to crafting clean, impactful designs, my work is a
+              testament to the fusion of skill and creativity.
+            </p>
+            <div className="bottom_line"></div>
+            <div className="social_media_container"></div>
+          </header>
+        </div>
+      </main>
+    </>
+  );
+}
