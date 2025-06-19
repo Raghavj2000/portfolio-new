a2c02a57943026f73e3550cc1cd1cbfa428d564f
Raghavv <raghavjayateerth@gmail.com>
2025-06-20 02:24:19 +0530

    added routes

--- app/page.js
+++ app/page.js
@@ -1,6 +1,34 @@
+import Link from "next/link";
 import "./page.css";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 
 export default function Home() {
+  const navlinks=[
+    {
+      id:1,
+      name:"Projects",
+      route:"/projects"
+    },
+    {
+      id:2,
+      name:"Services",
+      route:"/services"
+    },
+    {
+      id:3,
+      name:"Contact",
+      route:"/contact"
+    },
+    {
+      id:4,
+      name:"Use",
+      route:"/use"
+    },
+    
+  ]
+  
   return (
     <>
       <main className="main_body">
@@ -16,8 +44,22 @@ export default function Home() {
               testament to the fusion of skill and creativity.
             </p>
             <div className="bottom_line"></div>
-            <div className="social_media_container"></div>
+            <div className="social_media_container">
+              <FaGithub color="var(--font-primary-color)" size={30} />
+              <FaInstagram color="var(--font-primary-color)" size={30} />
+              <FaLinkedin color="var(--font-primary-color)" size={30} />
+              <FaXTwitter color="var(--font-primary-color)" size={30} />
+            </div>
           </header>
+          <div className="nav_container font-prata">
+            <ul>
+              {navlinks.map((link)=>(
+                <li key={link.id}>
+                  <Link href={link.route} className="nav_link">{link.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </main>
     </>
